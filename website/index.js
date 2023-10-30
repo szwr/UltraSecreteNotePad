29a1fef992f037e1df746619414e3dd303ee27d2
michu <119167055+skubichrupki@users.noreply.github.com>
2023-10-30 15:34:28 +0100

    front: roles, seperate functions

--- website/index.js
+++ website/index.js
@@ -1,28 +1,80 @@
-
+let role = 1;
+let text = "Ultra Secrete Note Pad"
+let index=0;
+let title = $("#title");
+let speed = 250;
+let randomTimeOut = Math.floor(Math.random() * speed);
 
 $(document).ready(function() {
 
-    console.log(randomTimeOut);
-    // ON PAGE LOAD
     typeText(text, index);
+    changeRole();
+
 });
 
+// SWITCH BETWEEN MESSAGE AND LINK ROLE
+const changeRole = () => {
 
-$("#submitButton").click(function() {
+    let submitButton = $("#submitButton");
+
+    submitButton.off("click");
+    
+    // MESSAGE ROLE
+    if (role == 1) {
+        
+        $(".message").hide();
+        $(".link").show();
+        console.log('MESSAGE');
+        submitButton.click( sendMessage );
+
+        role = 0
+    }
+    // LINK ROLE
+    else {
+
+        $(".message").show();
+        $(".link").hide();
+        console.log('LINK');
+        submitButton.click( sendLink );
+
+        role = 1;
+    }
+}
+
+$("#toggleRoleButton").click(changeRole);
+
+const typeText = (text, index) => {
+    
+    if (index < text.length) {
+        title.text(title.text() + text.charAt(index));
+        
+         index++;
+         randomTimeOut = Math.floor(Math.random() * speed);
+
+         setTimeout( () => {
+            typeText(text, index);
+
+         }, randomTimeOut);
+    }
+}
+
+// ROLE: MESSAGE -> SEND key-value pair: message-message to server, receive link
+function sendMessage() {
 
     // values from form
-    let link = $("#link").val();
-    let key = $("#key").val();
+    let message = $("#message").val();
+    let url = 'http//stronka.pl/';
+    let urlkey = '12345678';
     
     // display values to be sent to backend on the console
-    console.log(`SENT VALUE: ${link}`);
-    console.log(`SENT KEY: ${key}`);
+    console.log(`SENT MESSAGE: ${message}`);
     
     // POST method for now, can be later changed to JSON if needed
     // URL to be updated
     $.post( "http://localhost:3000/go-server-endpoint", {
-        link: link,
-        key: key,
+    
+        message: message
+        
         }, (response) => {
 
                 // response is JSON object return from the server
@@ -33,29 +85,53 @@ $("#submitButton").click(function() {
 
                 // display response in console / on website
                 // console.log(data.message);
-                // $("#response").html(response);
+                $("#response").html(`${url} ${urlkey}`);
             }).fail( (jqXHR, textStatus, errorThrown) => {
 
             console.log('ERROR HADNLING');
             console.log("jqXHR: " + jqXHR);
             console.log("textStatus: " + textStatus);
             console.log("errorThrown: " + errorThrown);
+
+            $("#response").html(`${url}${urlkey}`);
         })
-});
+}
 
-let text = "Ultra Secrete Note Pad"
-let index=0;
-randomTimeOut = Math.floor(Math.random() * 300);
+// ROLE: MESSAGE -> LINK key-value pair: link-link to server, receive message
+function sendLink() {
 
-const typeText = (text, index) => {
-    
-    if (index < text.length) {
-         $("#title").text($("#title").text() + text.charAt(index));
-         index++;
+     // values from form
+     let link = $("#link").val();
 
-         setTimeout( () => {
-            typeText(text, index);
+     let message = 'Siemano';
+     
+     // display values to be sent to backend on the console
+     console.log(`SENT LINK: ${link}`);
+     
+     // POST method for now, can be later changed to JSON if needed
+     // URL to be updated
+     $.post( "http://localhost:3000/go-server-endpoint", {
+     
+        link: link
+         
+         }, (response) => {
+ 
+                 // response is JSON object return from the server
+                 console.log(response);
+ 
+                 // converting JSON into JavaScript object
+                 let data = JSON.parse(response);
+ 
+                 // display response in console / on website
+                 // console.log(data.message);
+                 // $("#response").html(response);
+             }).fail( (jqXHR, textStatus, errorThrown) => {
+ 
+             console.log('ERROR HADNLING');
+             console.log("jqXHR: " + jqXHR);
+             console.log("textStatus: " + textStatus);
+             console.log("errorThrown: " + errorThrown);
 
-         }, 110);
-    }
+             $("#response").html(`${message}`);
+         })
 }
